1b890e2db3bfdb8b469d5e11e1c5cdb45f78c510
IvannikovI <mail@.ru>
2018-09-15 12:47:42 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+if (true){
+    let localData = "local";
+    var someData = "Data";
+    const constData = "const";
+}
+
+someData = "kdogpk";
+
+const globalArray=[];
+globalArray.push(1);
+let index = globalArray.indexOf(1);
+
+let obj = {
+    id: 72365,
+    name: "s[jhp[jk",
+    title: "kgokdgokd"
+};
+obj.addField = 125;
+globalArray.push(obj);
+
+function onButtonClick(){
+    let elem = document.getElementById("text");
+    elem.innerText = "changed";
+}
